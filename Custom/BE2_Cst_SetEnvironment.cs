06ec7df16fed3e82e2a68941677f560c59da42d4
mrcodycreed <83146709+mrcodycreed@users.noreply.github.com>
2026-06-09 12:10:03 -0500

    Replace obsolete environment manager lookup

--- Custom/BE2_Cst_SetEnvironment.cs
+++ Custom/BE2_Cst_SetEnvironment.cs
@@ -8,8 +8,15 @@ public class BE2_Cst_SetEnvironment : BE2_InstructionBase, I_BE2_Instruction
 
     public new void Function()
     {
-        if(manager == null)
-            manager = Object.FindObjectOfType<StoryCharacterManager>();
+        if (manager == null)
+            manager = Object.FindAnyObjectByType<StoryCharacterManager>();
+
+        if (manager == null)
+        {
+            Debug.LogWarning("No StoryCharacterManager found in scene.");
+            ExecuteNextInstruction();
+            return;
+        }
 
         string environment =
             Section0Inputs[0].StringValue;
